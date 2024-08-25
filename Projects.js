020092f20666f6202989fcc745bde712a72f99c8
NanoMars <174164929+NanoMars@users.noreply.github.com>
2024-08-25 18:21:34 +1000

    I sorta got it working, using a js file instead of json.

---
+++ Projects.js
@@ -0,0 +1,9 @@
+
+
+projects = {
+    "title": "Attaining Superintelligence w/ an AR Monocle",
+    "image": "images/FTC.jpg",
+    "description": "AI, human augmentation, software",
+}
+        
+    
