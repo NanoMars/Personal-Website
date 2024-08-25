57a4d010bf386b14c75dbf1b807d154fe513a707
NanoMars <174164929+NanoMars@users.noreply.github.com>
2024-08-25 20:05:10 +1000

    Programatically loading images, and titles and putting them in a flex column

--- index.js
+++ index.js
@@ -1,5 +1,83 @@
-Projects = require("./projects.json");
 
-import Projects from "./projects.json";
+const projects = [
+  { 
+    "title": "Building a competition robot",
+    "image": "images/FTC.jpg",
+  },
+  { 
+    "title": "Building a competition robot",
+    "image": "images/FTC.jpg",
+  },
+  { 
+    "title": "Building a competition robot",
+    "image": "images/FTC.jpg",
+  },
+  { 
+    "title": "Building a competition robot",
+    "image": "images/FTC.jpg",
+  },
+  { 
+    "title": "Building a competition robot",
+    "image": "images/FTC.jpg",
+  },
+  { 
+    "title": "Building a competition robot",
+    "image": "images/FTC.jpg",
+  },
+  { 
+    "title": "Building a competition robot",
+    "image": "images/FTC.jpg",
+  },
+  { 
+    "title": "Building a competition robot",
+    "image": "images/FTC.jpg",
+  },
+  { 
+    "title": "Building a competition robot",
+    "image": "images/FTC.jpg",
+  },
+  { 
+    "title": "Building a competition robot",
+    "image": "images/FTC.jpg",
+  },
+  { 
+    "title": "Building a competition robot",
+    "image": "images/FTC.jpg",
+  },
+  { 
+    "title": "Building a competition robot",
+    "image": "images/FTC.jpg",
+  },
+  
+  
+]
 
-console.log(Projects);
+document.addEventListener("DOMContentLoaded", loadProjects)
+
+function loadProjects() {
+    const projectsGallery = document.getElementById("projects-gallery")
+
+    for (const project of projects) {
+        const projectDiv = document.createElement("div")
+        projectDiv.classList.add("project")
+        projectsGallery.appendChild(projectDiv)
+
+        let projectImage = ""
+        let projectTitle = ""
+
+        for (const key in project) {
+            if (key === "image") {
+                projectImage = project[key]
+            } else if (key === "title" ) {
+                projectTitle = project[key]
+            } 
+        }
+        
+        projectDiv.innerHTML += `<div class="project-image" style="background: url(${projectImage}); background-size: cover">` 
+
+        const projectTitleDiv = document.createElement("div")
+        projectTitleDiv.classList.add("project-title")
+        projectDiv.appendChild(projectTitleDiv)
+        projectTitleDiv.innerHTML += `<h2>${projectTitle}</h2>`
+    }
+}
