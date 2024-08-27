d3fc1b0dca1c50d11e9c73d1beddf028d27cbbf8
NanoMars <174164929+NanoMars@users.noreply.github.com>
2024-08-27 13:03:15 +1000

    Added projects with new names and stuff to the database, gotta add images and content and stuff to them though

--- Projects.js
+++ Projects.js
@@ -11,7 +11,41 @@ const projects = [
     "ID": "Group-Shuffle",
     "inpage-title": "Group Shuffle",
     "github": "https://github.com/NanoMars/Group-Shuffle",
-  }
+  },
+  {
+    "title": "Branding and animation for design agency",
+    "image": "images/GroupShuffle.png",
+    "ID": "gimbit-creative",
+    "inpage-title": "GIMBIT creative - concept design",
+  },
+  {
+    "title": "Making chess in my browser",
+    "image": "images/GroupShuffle.png",
+    "ID": "WebChess",
+    "inpage-title": "WebChess",
+    "github": "https://github.com/NanoMars/webchess",
+  },
+  {
+    "title": "Clicker game with python",
+    "image": "images/GroupShuffle.png",
+    "ID": "JewelHarvest",
+    "inpage-title": "Jewel Harvest",
+    "github": "https://github.com/NanoMars/JewelHarvest",
+  },
+  {
+    "title": "Fully 3D printable chess set",
+    "image": "images/GroupShuffle.png",
+    "ID": "Cad-Chess",
+    "inpage-title": "3D Printed Chess",
+    "github": "https://github.com/NanoMars/Cad-Chess",
+  },
+  {
+    "title": "Collection of 9 unique mini-games in python",
+    "image": "images/GroupShuffle.png",
+    "ID": "MiniGames",
+    "inpage-title": "Pygame Collection",
+    "github": "https://github.com/NanoMars/mini-pygame-collection",
+  },
   ]
 
 export {projects}
